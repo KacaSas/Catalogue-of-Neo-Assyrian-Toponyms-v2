c0d116b228a6e5bc47450145f783dca3fbaa009b
KacaSas <katerina.saskova@gmail.com>
2025-06-11 21:44:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,6 @@ import base64
 import sys
 import path
 
-dir = path.Path(__file__).abspath()
-sys.append.path(dir.parent.parent)
-
 st.set_page_config(page_title='Cuneiform signs', page_icon='resources/icon/icon.png', layout='wide')  # change favicon and page title
 
 def clearSignListForm():
@@ -39,7 +36,7 @@ option = st.selectbox('Main menu', ('Cuneiform signs', 'Crop and resize images')
 
 if option == 'Cuneiform signs':
 	st.header('Cuneiform') 
-	data = pd.read_csv('./SignList/SignList.csv')
+	data = pd.read_csv('kacasa/kliny/main/SignList/SignList.csv')
 	data = data.fillna(value='+++++++')
 
 	st.write('<b><font style="font-size: 21px">Search sign</font></b> (case insensitive, regular expressions allowed):', unsafe_allow_html=True)
