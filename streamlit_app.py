de88f45d56047597be1fe564d7a11c33635461bb
KacaSas <katerina.saskova@gmail.com>
2025-06-12 07:51:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -235,7 +235,7 @@ if option == 'Cuneiform signs':
 		st.markdown('– Cuneify REPL (Jon Knowles): https://amazing-chandrasekhar-e6c92b.netlify.app/index.html.<br>– CuneifyPlus (Tom Gillam): https://cuneify.herokuapp.com/.<br>– Cuneify (Steve Tinney): http://oracc.museum.upenn.edu/saao/knpp/cuneiformrevealed/cuneify/.<br>– KUR.NU.GI4.A – Cuneiform Script Analyzer (uyum): https://kurnugia.web.app/.<br>– GI-DUB – Sumerian Cuneiform Input Aid (uyum): https://qantuppi.web.app/.', unsafe_allow_html=True)
 
 if option == 'Something else':
-	st.header('Something else') 
+#	st.header('Something else') 
 
 #	def text_field(label, columns=None, **input_params):
 #		column1, column2 = st.columns(columns or [1, 12])
